20affba1e9a3dd121e41f061c3ec314bd3c1a3ed
gugu <gustavocorrea2032@gmail.com>
2019-06-25 12:36:06 -0300

    DoWhile1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DoWhile1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            do {
+                Console.WriteLine (n); 
+                n++;
+            } while (n <= 10);
+        }
+    }
+}
